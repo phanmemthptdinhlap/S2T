1042cce69492333645f621f696f1211323f14446
phanmemthptdinhlap <phanmemthptdinhlap@gmail.com>
2022-04-20 09:37:38 +0700

    save

---
+++ rec.py
@@ -0,0 +1,14 @@
+import sounddevice as sd
+import numpy as np
+import soundfile as sf
+
+fs=44100
+duration = 5  # seconds
+myrecording = sd.rec(duration * fs, samplerate=fs, channels=2,dtype='float64')
+print ("Recording Audio")
+sd.wait()
+print ("Audio recording complete , Play Audio")
+sd.play(myrecording, fs)
+sd.wait()
+print ("Play Audio Complete")
+sf.write('test.wav',myrecording,fs)
